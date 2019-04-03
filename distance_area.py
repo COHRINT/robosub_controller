87792fcb718d8629da570f3b034b007887c10174
Jeremy <amuesing1@gmail.com>
2019-04-03 14:58:00 -0600

    file to get moment arms and areas

---
+++ distance_area.py
@@ -0,0 +1,24 @@
+from __future__ import division
+import numpy as np
+import skimage
+import matplotlib.pyplot as plt
+
+def mask_image(image):
+    '''creates an image mask to compute area
+    and distances'''
+    pass
+
+def area(image,hor_dim):
+    '''computes surface area given by image
+    hor_dim-the distance in desired units
+    along the horizontal axis'''
+    pass
+
+def distance(image,hor_dim):
+    '''computes moment arm distance given by image
+    hor_dim-the distance in desired units
+    along the horizontal axis'''
+    pass
+
+if __name__ == '__main__':
+    pass
